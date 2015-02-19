1d2b55fd46217cf7eece87cca5c606eacd86aff9
Twan Keijzer <twankeijzer@hetnet.nl>
2015-02-19 10:58:05 +0100

    lift

--- Python_code/NumericalSimulation/Lift.py
+++ Python_code/NumericalSimulation/Lift.py
@@ -1,9 +1,9 @@
 def Lift(z,liftDist,l2,l1):
     import numpy as np
     h = liftDist/l2
-    if z<l1: Lift = 0
+    if z<l1 and z>0: Lift = 0
     if z>l1 and z<l1+l2: Lift = (z-l1)/l2*h
-    else: raise IOError('thicknesses are too big')
-return Lift
+    else: raise IOError('z outside wing')
+    return Lift
 #unit test
-#    Lift(1,)
+print Lift(0,1,1,1)
