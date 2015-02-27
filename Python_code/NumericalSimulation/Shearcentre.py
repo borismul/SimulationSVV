b25bd6080f7ed64b36cb74940bb2b64bd5c83c10
Karlo <k.rado@hotmail.com>
2015-02-27 02:16:11 +0100

    numpy errors are most likely present, but python x,y isn't fully functional. Anyway this is the completed shear centre. It was done in 2 hours. Not the most efficient :/

--- Python_code/NumericalSimulation/Shearcentre.py
+++ Python_code/NumericalSimulation/Shearcentre.py
@@ -1,8 +1,7 @@
 #Function to calculate the shear flow in a crossection. 
 def Shearcentre(chordLength, I, tFront, tRear, tBottom, tTop, stepsXY):
-    import numpy as np
     #Inertias are defined:
-    I[0] = Ixx
+    Ixx = I[0]
     #Since the section is cut at symmetry it has to have an equal number of steps on top and bottom
     if stepsXY%2 == 0:
         steps = (stepsXY)/2
@@ -11,7 +10,7 @@ def Shearcentre(chordLength, I, tFront, tRear, tBottom, tTop, stepsXY):
     #Defining start points
     qb = 0
     iqs0 = 0
-    qblist = np.array([])
+    qblist = []
     dy = 0.1*chordLength/stepsXY
     dx = 0.5*chordLength/stepsXY
 #The section is cut at the axis of symmetry, i.e. x= -0.25c y=0 wrt our reference system
@@ -19,23 +18,31 @@ def Shearcentre(chordLength, I, tFront, tRear, tBottom, tTop, stepsXY):
     #From cut to top panel
     for y in np.linspace(0,0.05*chordLength,steps):
         qb -= 1/Ixx * tRear*y*dy
-        iqs0 -= qb*dy
+        iqs0 += qb*dy
     #From rear to front panel
     for x in np.linspace(0.,0.5*chordLength,stepsXY):
         qb -= 1/Ixx * tTop*(0.05*chordLength)*dx
-        iqs0 -= qb*dx
+        iqs0 += qb*dx
         qblist.append(qb)
+        list1=len(qblist)
     #Halfway from the top to the bottom panel
     for y in np.linspace(0,0.05*chordLength,steps):
         qb -= 1/Ixx * tFront*(0.05*chordLength-y)*dy
-        iqs0 -= qb*dy
+        iqs0 += qb*dy
         qblist.append(qb)
+        list2=len(qblist)
     #the other parts follow from symmetry.
     #qs0 is now divided by the other integral to yield qs0.
-    qs0 = iqs0/(0.5*chordLength + 0.1*chordLength)
+    qs0 = -iqs0/(0.5*chordLength + 0.1*chordLength)
     #Now we take moments about the cut. Symmetry now comes into play
     #shearx, is the X-coordinate of the shear centre wrt the rear panel
-    Q = 0
+    Qx = 0
     for x in np.linspace(0.,0.5*chordLength,stepsXY):
-        Q += 0
-    shearx = 2*Q
+        Qx += (qs0+qblist[0,list1-1])*0.05*chordLength*dx
+    for y in np.linspace(0,0.05*chordLength,steps):
+        Qx += (qs0+qblist[list1-1, list2-1])*0.5*chordLength*dy
+    shearx = 2*Qx
+    return shearx
+
+import numpy as np
+print(Shearcentre(5, [1,0,0], 1, 1, 1, 1, 5))
