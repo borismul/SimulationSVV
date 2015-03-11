489aba0b8eeff84cdafc5ab1c622868908b50024
Twan Keijzer <twankeijzer@hetnet.nl>
2015-03-11 12:13:30 +0100

    weer

--- Python_code/NumericalSimulation/Main.py
+++ Python_code/NumericalSimulation/Main.py
@@ -124,4 +124,3 @@ PlotImportantGraphs(stepsZ,l1,l2,shearForceArray,momentArray,normalStressArray,s
 #PlotUnitTests(stepsZ,l1,l2,IArray,liftArray,coordinates,normalStressArray,shearStressArray,torqueArray,ys,plt)
 #ValidationData(normalStressArray,shearStressArray,l1,l2,stepsZ,stepsXY)
 PlotVerificationData(normalStressArray,shearStressArray,stepsXY,l1,l2)
->>>>>>> 52d463c0d41f8f202181468e5d1924945aca47b3
